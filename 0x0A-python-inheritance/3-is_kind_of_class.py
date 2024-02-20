fbbdeddf6d94cecebd3be7b8127aea03dafac4f7
emehdii <mehdi.mahdaoui42@gmail.comgit config --global mehdi.mahdaoui42@gmail.com>
2024-02-20 16:51:52 +0100

    0x0A

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,8 @@
-!/usr/bin/python3
-"""Module containing is_kind_of_class method"""
+#!/usr/bin/python3
+"""3-is_kind_of_class
+"""
 
 
 def is_kind_of_class(obj, a_class):
-    """returns True if the object is an instance of, or if the
-    object is an instance of a class that inherited from
-    the specified class ; otherwise False """
-    return isinstance(obj, a_class)
+    """ Compare is obj is an instance of a_class """
+    return isinstance(obj, a_class):
